b35a97f520bf830aa14d8013a18c72fa9e0c8927
Gonçalo Pereira <pereiragoncalo072@gmail.com>
2023-12-05 02:17:58 +0000

    [ADD] delete method

--- BDTech/backend/views.py
+++ BDTech/backend/views.py
@@ -1,8 +1,7 @@
-from django.http import HttpResponse, HttpResponseNotFound
+from django.http import HttpResponse, HttpResponseNotFound, Http404 
 from django.db import connection, ProgrammingError
 from django.template import loader
 from django.shortcuts import render, redirect
-
 
 def masterPage(request):
     template = loader.get_template("masterPage.html")
@@ -26,29 +25,38 @@ def function_exists(table_name):
     return True
 
 def generic_list(request, table_name):
-    function_table_name = f"{table_name}_get(1,0)"
+    function_table_name = f"{table_name}_get_list(1,0)"
     if not function_exists(function_table_name):
-        return redirect('404/')
+        raise Http404("Function not found")
 
     with connection.cursor() as c:
         query = f"SELECT * FROM {function_table_name}"
         c.execute(query)
         rows = c.fetchall()
         columns = [desc[0] for desc in c.description]
+    
+    rows_as_dicts = [dict(zip(columns, row)) for row in rows]
 
+    for row in rows_as_dicts:
+        row['id_field'] = row[f"n_{table_name}"]
+    
     context = {"table_data": rows, "columns": columns, "table_name": table_name}
     return render(request, "list.html", context)
 
 
+def delete_record(request, table_name, record_id):
+    try:
+        with connection.cursor() as cursor:
+            cursor.callproc('delete_record', [table_name, record_id])
 
+            results = cursor.fetchone()
 
+            if results[0] == 1:
+                return redirect('/'+table_name+'/list') 
+            else:
+                raise Http404(results[1])
 
-# def utilizador_list(request):
-#     from django.db import connection
+    except Exception as e:
+        raise Http404(str(e))
 
-#     c = connection.cursor()
-#     c.execute("select * from utilizador_get(1,0)")
-#     row = c.fetchall()
-
-#     context = {"list_utilizador": row, "context": Utilizador.objects.all}
-#     return render(request, "list.html", context)
+    return render(request, 'delete_record.html', {'table_name': table_name, 'record_id': record_id})
--- BDTech/core/urls.py
+++ BDTech/core/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from backend.views import  dashboard, generic_list, error404
+from backend.views import  dashboard, generic_list, error404, delete_record
 from frontend.views import  index, kanban
 
 urlpatterns = [
@@ -9,4 +9,5 @@ urlpatterns = [
     path("<str:table_name>/list/", generic_list, name='generic_list'),
     path("index/", index),
     path("kanban/", kanban),
+    path('delete_record/<str:table_name>/<int:record_id>/', delete_record, name='delete_record'),
 ]
