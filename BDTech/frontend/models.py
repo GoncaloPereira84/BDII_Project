2fd183ac0eb1b55c1079f3e92608183a77a470dc
unknown <gabriela_nokax@hotmail.com>
2023-12-11 18:23:01 +0000

    index

--- BDTech/frontend/models.py
+++ BDTech/frontend/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
 # Create your models here.
+# models.py
+
+class Equipamento(models.Model):
+    id_equipamento = models.AutoField(primary_key=True)
+    descricao = models.TextField()
+    imagem = models.ImageField()
+    preco = models.TextField()
+    nome = models.TextField()
+
+    def __str__(self):
+        return self.nome  # Certifique-se de que existe um campo 'nome' no seu modelo Equipamento
+
+    class Meta:
+        db_table = 'equipamento'
+""" 
+class TipoEquipamento(models.Model):
+    id_tipoequipamento = models.AutoField(primary_key=True)
+    nome = models.CharField(max_length=50)
+    descricao = models.CharField(max_length=100)
+
+class Estado(models.Model):
+    id_estado = models.AutoField(primary_key=True)
+    nome = models.CharField(max_length=50)
+    descricao = models.CharField(max_length=100)
+
+class Moeda(models.Model):
+    id_moeda = models.AutoField(primary_key=True)
+    nome = models.CharField(max_length=50)
+    descricao = models.CharField(max_length=100) """
--- BDTech/frontend/views.py
+++ BDTech/frontend/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse, HttpResponseNotFound
 from django.db import connection, ProgrammingError
 from django.template import loader
 from django.shortcuts import render, redirect
+from .models import Equipamento
 
 # Create your views here.
 
@@ -10,9 +11,23 @@ def masterPageFront(request):
     return HttpResponse(template.render())
 
 def index(request):
-    template = loader.get_template("index.html")
-    return HttpResponse(template.render())
+    ultimos_equipamentos = Equipamento.objects.order_by('id_equipamento')[:6]
+    marcas = [
+        {'nome': 'Apple', 'logo_url': 'https://th.bing.com/th/id/R.24e62b73004a5fb7a7a5d9c26963ed07?rik=nzIAxmu0duWlgA&riu=http%3a%2f%2fwww.zdnet.de%2fwp-content%2fuploads%2f2013%2f05%2fapple-logo-schwarz.jpg&ehk=q59%2b69Oq6FABvF0DbfJTGBD7m4IqvKMyNFPSzy8pRRg%3d&risl=&pid=ImgRaw&r=0'},
+        {'nome': 'Lenovo', 'logo_url': 'https://th.bing.com/th/id/OIP.IRDIAwBmlFyff6g1EuFjXwHaEZ?rs=1&pid=ImgDetMain'},
+        {'nome': 'Asus', 'logo_url': 'https://th.bing.com/th/id/OIP.M78UplVnuECVc6nFgq8p1wHaFj?w=640&h=480&rs=1&pid=ImgDetMain'},
+        {'nome': 'MSI', 'logo_url': 'https://th.bing.com/th/id/OIP.S9Dvja4R5tcjaPYe381o0gHaEK?rs=1&pid=ImgDetMain'},
+       
+
+    ]
+    return render(request, 'index.html', {'equipamentos': ultimos_equipamentos, 'marcas': marcas})
 
 def kanban(request):
     template = loader.get_template("kanban.html")
     return HttpResponse(template.render())
+
+
+
+
+
+
