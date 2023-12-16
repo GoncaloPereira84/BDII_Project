88cdac9875607db1a97ce534d8f9c480987440cc
unknown <gabriela_nokax@hotmail.com>
2023-12-16 22:49:54 +0000

    kanban

--- BDTech/frontend/models.py
+++ BDTech/frontend/models.py
@@ -7,24 +7,27 @@ class User(models.Model):
     def __str__(self):
         return self.username
 
+class TipoEquipamento(models.Model):
+    id_tipoequipamento = models.AutoField(primary_key=True)
+    nome = models.CharField(max_length=50)
+    descricao = models.CharField(max_length=100)
+
 class Equipamento(models.Model):
     id_equipamento = models.AutoField(primary_key=True)
     nome = models.CharField()
     descricao = models.TextField()
     imagem = models.ImageField()
     preco = models.TextField()
-
+    id_tipoequipamento = models.ForeignKey(TipoEquipamento,null=True, on_delete=models.CASCADE, db_column='id_tipoequipamento')
+ 
     def __str__(self):
         return self.nome
 
     class Meta:
         db_table = 'equipamento'
-""" 
-class TipoEquipamento(models.Model):
-    id_tipoequipamento = models.AutoField(primary_key=True)
-    nome = models.CharField(max_length=50)
-    descricao = models.CharField(max_length=100)
 
+
+""" 
 class Estado(models.Model):
     id_estado = models.AutoField(primary_key=True)
     nome = models.CharField(max_length=50)
