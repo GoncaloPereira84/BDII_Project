278b7b2c8b7a4cb1f074cdc2a66cceece37a1465
marqueshj <marqueshj@hjmsoft.com>
2024-01-19 18:06:23 +0000

    Update views.py

--- BDTech/frontend/views.py
+++ BDTech/frontend/views.py
@@ -98,8 +98,6 @@ def logout_utilizador(request):
 # view login
 @csrf_exempt
 def fulllogin(request):
-    origin_header = request.META.get('HTTP_ORIGIN')
-    print('Origin Header:', origin_header)
 
     if request.method == "POST":
         username = request.POST["username"]
